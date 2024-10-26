d34fc8aae37a40c413a989e3ce5a10525820f352
psvsai <pasupuletisvsm245@gmail.com>
2024-10-26 10:04:03 +0530

    Day 2 Practice

---
+++ Day2Practice.js
@@ -0,0 +1,38 @@
+//Basic of JavaScript 
+let a = 20;
+var b = 50;
+const c = a+b;
+console.log(c);
+let age = 24;
+let adding  = age + 3;
+console.log(adding); 
+// Function calling medthod
+function name (){
+    console.log("Recollecting the JS basic");
+}
+name();
+//function returning medtod
+function multiply(a,b){
+    return(a*b);
+}
+console.log(multiply(2,3));//or u can declare a variable.
+let off = multiply(3,3);
+console.log(off);
+
+// ES6
+const sum = (a,b) => a+b;
+let addingTwoNum = sum(8,8);
+console.log(addingTwoNum);
+
+
+
+
+
+
+
+
+
+
+
+
+
