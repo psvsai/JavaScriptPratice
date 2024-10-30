c154e1efa90da3659b2d1944b238aa2672efe611
psvsai <pasupuletisvsm245@gmail.com>
2024-10-30 21:00:58 +0530

    Day 4 Practice

---
+++ Day4Practice.js
@@ -0,0 +1,48 @@
+//    30-20-2024   // practing datatypes 
+var A = 3;
+var B = 5;
+var C = A+B;
+console.log('Sum of two numbers are = '   + C);
+
+let x = 5;
+let y = 10;
+let z = x*y;
+console.log('product of two number is  = ' + z);
+
+
+const a = 10;
+const b = 20 ;
+const c = 30;
+console.log('Subtracting two number  = ' + c);
+
+
+// methods Practice
+function  sum(){
+    console.log(10+10);
+  } 
+  sum();
+  
+  function num(a,b){
+    console.log((a+b));
+  }
+  let c1 = num(5,6);
+  
+  function onepiece(x,y){
+    return x+y;
+  }
+  var sunny = onepiece(100,200);
+  console.log(sunny);
+  
+  function str(String){
+    return String;
+  }
+  let hlo = str('sai');
+  console.log(hlo);
+  
+  function hi(String1, String2){
+    return String1+String2;
+  }
+  let name = hi('psvsai ', 'mahesh');
+  console.log('my name is ' + name);
+
+// Async and Await function
