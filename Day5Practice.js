13c6ead8c5e0e6ee7766598f6a87b59f256a50bb
psvsai <pasupuletisvsm245@gmail.com>
2024-10-31 23:12:09 +0530

    Day5 Practice on array unshift and push

---
+++ Day5Practice.js
@@ -0,0 +1,21 @@
+// JS basic arrays practice
+// adding array in the beginning the keyword used is (unshift) 
+let fruits = ["apple","banana","cherry","watermelon"];
+fruits.unshift("guava");
+console.log(fruits);
+
+let cars = ["suzuki","hyundai","honda"];
+cars.unshift("tata");
+console.log(cars);
+
+let anime=["one piece","naruto","dargon ballz"];
+anime.unshift("deathnote");
+console.log(anime);
+
+let name = ["mahesh","sai","psvsai"];
+name.push("pasupuleti");
+console.log(name);
+
+const family =["father","brother","myself"]; 
+family.push("amma","akka");
+console.log(family);
