79f7d14afb2e8ace037bcbc9a3a04af87863db99
psvsai <pasupuletisvsm245@gmail.com>
2024-12-10 22:39:56 +0530

    21,22,23

---
+++ Merge/arrayMerge.js
@@ -0,0 +1,14 @@
+let a = [1,2,3];
+let b = [4,5,6];
+let c = [...a,  ...b, 7];
+console.log(c);
+
+var array0 = [0,1,2,3,4,5,6,7,8,9];
+var array1 =  [10,11,12,13,14,15,16,18,19];
+var array = [...array0, ...array1,20];
+console.log(array);
+
+var nam0 = ["Pasupuleti Sri Venkata"];
+var nam1 = ["Sai Mahesh"];
+var name = [...nam0, ...nam1];
+console.log(name);
